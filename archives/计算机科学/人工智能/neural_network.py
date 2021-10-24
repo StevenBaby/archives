c1688023c763d42f664903995ee8bdd011af43a0
Steven Kang <kangweibaby@163.com>
2021-10-24 15:11:06 +0800

    update neural network

--- archives/计算机科学/人工智能/neural_network.py
+++ archives/计算机科学/人工智能/neural_network.py
@@ -1,17 +1,13 @@
 # coding=utf-8
 
-def and_gate(x1, x2):
+def AND(x1, x2):
     s = x1 * 1 + x2 * 1
     if s >= 2:
         return 1
     else:
         return 0
 
-# def and_gate(x1, x2):
-#     return int(x1 and x2)
-
-
-def or_gate(x1, x2):
+def OR(x1, x2):
     s = x1 * 1 + x2 * 1
     if s >= 1:
         return 1
@@ -19,7 +15,7 @@ def or_gate(x1, x2):
         return 0
 
 
-def and_not_gate(x1, x2):
+def NAND(x1, x2):
     s = x1 * (-1) + x2 * (-1)
     if s >= -1:
         return 1
@@ -27,18 +23,14 @@ def and_not_gate(x1, x2):
         return 0
 
 
-def not_gate(x):
-    return and_not_gate(x, 1)
-
-
-def xor_gate(x1, x2):
-    v1 = and_gate(x1, not_gate(x2))
-    v2 = and_gate(not_gate(x1), x2)
-    return or_gate(v1, v2)
+def XOR(x1, x2):
+    s1 = NAND(x1, x2)
+    s2 = OR(x1, x2)
+    return AND(s1, s2)
 
 
 def main():
-    gate = xor_gate
+    gate =  XOR
     print(gate(0, 0))
     print(gate(0, 1))
     print(gate(1, 0))
